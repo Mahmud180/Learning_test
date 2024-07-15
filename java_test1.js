b36e7f1717dce0b6f28d2f585af716e98dda81aa
Mahmud Hasan Shawon <100403245+Mahmud180@users.noreply.github.com>
2024-07-15 11:50:58 +0600

    Update java_test1.js

--- java_test1.js
+++ java_test1.js
@@ -1,2 +1,8 @@
-console.log('Hello World');
-console.log('hi');
+var carcolor1 = "red";
+var carcolor2 = "blue";
+var carcolor3 = "yellow";
+var num1 = 5;
+var num2 = 7;
+
+console.log(num1+num2);
+
