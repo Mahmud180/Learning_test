a9bf600471b620154f4bd08d3a167aa82199d4a8
Mahmud Hasan Shawon <100403245+Mahmud180@users.noreply.github.com>
2024-08-09 09:33:16 +0600

    Update java_test4.js

--- java_test4.js
+++ java_test4.js
@@ -1,9 +1,9 @@
-//var x = 7.12321;
-//var y = x.toPrecision(3);
-//var z = x.toPrecision();
+var x = 7.12321;
+var y = x.toPrecision(3);
+var z = x.toPrecision();
 
-//console.log(y);
-//console.log(z);
+console.log(y);
+console.log(z);
 
 var a = 123;
 b = a.toString();
